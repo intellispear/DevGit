023afea1d528917a6d29a7113985710e4ee70332
intellispear <61251015+intellispear@users.noreply.github.com>
2024-01-05 16:28:25 +0000

    First funny code

---
+++ SuperImportaint.docx.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+def play_song():
+    # URL of the song on YouTube
+    song_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    # Open the web browser
+    webbrowser.open(song_url)
+
+# Call the function
+play_song()
